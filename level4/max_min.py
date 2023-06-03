f52429367c2fb061c2cf3ae12f27b542e847c4a6
junguk <junkuk9503@gmail.com>
2023-06-03 23:11:22 +0900

    repush

--- level4/max_min.py
+++ level4/max_min.py
@@ -1,12 +1,3 @@
 num = int(input())
 num_list = list(map(int, input().split()))
-max = -1000000
-min = 1000000
-for i in range(num):
-    if(num_list[i]>max):
-        max = num_list[i]
-    elif(num_list[i]<min):
-        min = num_list[i]
-    else:
-        continue
-print(min,max)
+print(min(num_list),max(num_list))
