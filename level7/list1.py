d8f7aa092db4eed348724d357b7353deceea9b23
junguk <junkuk9503@gmail.com>
2024-01-12 00:59:41 +0900

    commit

---
+++ level7/list1.py
@@ -0,0 +1,12 @@
+def solution(food):
+    num = len(food) - 1
+    str1 = list()
+    arr = list()
+    for i in range(num):
+        for j in range(food[i+1] // 2):
+            str1.append(i+1)
+    arr = str1 + str1[::-1]
+    arr.insert(len(arr)//2,0)
+    return "".join(map(str,arr))
+
+print(solution([1, 3, 4, 6]))
