62a21cd7a881c28e601cf6d619e11cc65f9ff4c5
junguk <junkuk9503@gmail.com>
2024-01-19 21:47:05 +0900

    commit

---
+++ level7/combination.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def solution(sequence, k):
+    arr = list()
+    sequence1 = {num1: num2 for num2, num1 in sequence}
+    for i in range(len(sequence)):
+        for num in combinations(sequence,i+1):
+            sum1 = sum(num)
+            if(sum1 == k):
+                arr.append(sequence1[num[0]])
+                arr.append(sequence1[num[-1]])
+                break
+        if(sum1 == k):
+            break
+    return arr
+
+print(solution([1, 2, 3, 4, 5],7))
